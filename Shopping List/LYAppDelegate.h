52cda805be50197a6f0b2d026f1b85d22256da8b
LI Yi <liyi@smartisan.cn>
2014-06-01 10:27:22 +0800

    Initial Commit

---
+++ Shopping List/LYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LYAppDelegate.h
+//  Shopping List
+//
+//  Created by LI Yi on 6/1/14.
+//  Copyright (c) 2014 Flaneur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Shopping List/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Shopping List
+//
+//  Created by LI Yi on 6/1/14.
+//  Copyright (c) 2014 Flaneur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LYAppDelegate class]));
+    }
+}
